19b8fd33e747acd36afea25f4c00438043e6d707
sabih khan <sabihop56@gmail.com>
2026-05-09 19:40:41 +0500

    fixed environemnt configuration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,9 @@
+/// <reference types="vite/client" />
+
 interface ImportMetaEnv {
   readonly VITE_BASE_URL: string
+  readonly VITE_CLOUDINARY_CLOUD_NAME: string
+  readonly VITE_CLOUDINARY_UPLOAD_PRESET: string
 }
 
 interface ImportMeta {
